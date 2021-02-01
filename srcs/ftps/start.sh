6a7babf02893ae565057c0bf80ba549132e9c8b7
LT <jiloh55@gmail.com>
2021-02-01 15:07:29 +0100

    ftp serv done

---
+++ srcs/ftps/start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+## Conf ftps 
+
+cd /etc/vsftpd/
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ftps.pem -out ftps.pem -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com"
+mkdir -p /home/vsftpd/admin/ 
+chmod 777 /home/vsftpd/admin/
+echo "admin:$(openssl passwd -1 root)" >> /etc/vsftpd/virtual_users
+chmod 600 /etc/vsftpd/virtual_users
+
+## Start services &  keep container running
+
+vsftpd /etc/vsftpd/vsftpd.conf
--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -6,13 +6,18 @@ mkdir -p /var/lib/mysql /run/mysqld
 
 # install and start services
 mysql_install_db -u root
-/usr/bin/mysqld -u root --console &
+mysqld -u root &
 sleep 5
 
-mysql -u root -e "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+if ! mysql -u root -e 'USE wordpress'; then
+	mysql -u root -e "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+	mysql -u root wordpress < /wordpress-tmp.sql
+fi
+
+# check this line, maybe doesnt work?
 mysql -u root -e "GRANT ALL ON wordpress.* TO 'wordpress_user'@'localhost' IDENTIFIED BY 'password';"
-mysql -u root -e "FLUSH PRIVILEGES"
+mysql -u root -e "FLUSH PRIVILEGES;SHUTDOWN"
 
 ## Start services &  keep container running
 
-sleep infinity
+mysqld -u root
