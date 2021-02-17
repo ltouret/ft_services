c2518849dd23a63cbef933edfc6b817c2f1b86d3
LT <jiloh55@gmail.com>
2021-02-17 11:01:36 +0100

    done

--- srcs/nginx/start.sh
+++ srcs/nginx/start.sh
@@ -9,4 +9,13 @@ rm server.*
 
 ## Start services &  keep container running
 telegraf &
-nginx -g 'daemon off;'
+nginx
+
+while true
+do
+	sleep 1
+	if [ $(ps | grep -v grep | grep nginx | wc -l) -eq 0 ] || [ $(ps | grep -v grep | grep telegraf | wc -l) -eq 0 ]
+	then
+		exit
+	fi
+done
