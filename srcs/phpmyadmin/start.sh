30209873b285920f82b31450e8603d4fd5be2fbb
LT <jiloh55@gmail.com>
2021-05-01 18:39:24 +0200

    fixed bug in phpmyadmin

--- srcs/phpmyadmin/start.sh
+++ srcs/phpmyadmin/start.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-## Conf phpMyAdmin
-wget https://files.phpmyadmin.net/phpMyAdmin/4.9.5/phpMyAdmin-4.9.5-all-languages.tar.gz 
-tar xzvf phpMyAdmin-4.9.5-all-languages.tar.gz
-mv phpMyAdmin-4.9.5-all-languages/* /www/phpmyadmin/
-rm phpmyadmin/config.sample.inc.php
-rm -rf phpMyAdmin-4.9.5-all-languages.tar.gz phpMyAdmin-4.9.5-all-languages
-
 ## Start services &  keep container running
 telegraf &
 php-fpm7
